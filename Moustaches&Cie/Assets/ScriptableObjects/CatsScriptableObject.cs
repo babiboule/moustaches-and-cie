da5e7d1e4bfc21ec813c9741e53b3fcedfcef6c2
babiboule <baptiste.debouche@gmail.com>
2024-04-17 15:59:04 +0200

    Creating cats infos structure

---
+++ Moustaches&Cie/Assets/ScriptableObjects/CatsScriptableObject.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "CatsInfos", menuName = "ScriptableObjects/CatsScriptableObject", order = 1)]
+    public class CatsScriptableObject : ScriptableObject
+    {
+        public enum Nature
+        {
+            Jovial,
+            Peureux,
+            Agressif
+        }
+        [System.Serializable] public struct Cat
+        {
+            public Image picture;
+            public string name;
+            public float age;
+            public string race;
+            public bool sick;
+            public Nature nature;
+            public bool outdoor;
+            public bool animals;
+            public bool adopted;
+        }
+
+        public List<Cat> cats;
+    }
+}
---
+++ Moustaches&Cie/Assets/Scripts/CatManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using ScriptableObjects;
+using UnityEngine;
+
+public class CatManager : MonoBehaviour
+{
+    public CatsScriptableObject cats;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
